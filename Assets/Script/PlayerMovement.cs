f06205628017c6cd650d0137123c6fb157ade97f
jqlnet <toniliang@cpp.edu>
2025-10-08 23:50:37 -0700

    added character animations for walking and idling

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,8 +7,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
-    void Start() { rb = GetComponent<Rigidbody2D>(); }
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         rb.velocity = moveInput * moveSpeed;
@@ -16,7 +21,18 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true); // animator sets walking to true at first when we move, then will check boolean value below.
+        
+        if (context.canceled) // when we stop walking we tell the animator to switch back to idle.
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x); // the x input is whatever moveinput we use for x
+        animator.SetFloat("InputY", moveInput.y); // the y input also follows the same rule
     }
 }
 
