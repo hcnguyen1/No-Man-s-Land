ccba156b99a733914be5bf1a50bccd9a96296da8
jqlnet <toniliang@cpp.edu>
2025-12-12 19:15:40 -0800

    added currency gold coin

--- Assets/Script/PickUpSystem/CurrencyItem.cs
+++ Assets/Script/PickUpSystem/CurrencyItem.cs
@@ -6,23 +6,6 @@ public class CurrencyItem : MonoBehaviour
     [SerializeField]
     private int currencyValue = 100; // How much currency this item gives
 
-    [SerializeField]
-    private AudioSource audioSource;
-
-    [SerializeField]
-    private float duration = 0.3f;
-
-    [SerializeField]
-    private Sprite currencySprite; // Optional: set a sprite for the currency
-
-    private void Start()
-    {
-        if (currencySprite != null)
-        {
-            GetComponent<SpriteRenderer>().sprite = currencySprite;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
@@ -42,22 +25,7 @@ public class CurrencyItem : MonoBehaviour
 
     private IEnumerator AnimateCurrencyPickup()
     {
-        if (audioSource != null)
-        {
-            audioSource.Play();
-        }
-
-        Vector3 startScale = transform.localScale;
-        Vector3 endScale = Vector3.zero;
-        float currentTime = 0;
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            transform.localScale = Vector3.Lerp(startScale, endScale, currentTime / duration);
-            yield return null;
-        }
-
         Destroy(gameObject);
+        yield return null;
     }
 }
